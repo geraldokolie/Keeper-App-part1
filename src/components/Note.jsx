dc9912eac6dd9cbda91e841110ea55110b67a7ba
geraldokolie <101581930+geraldokolie@users.noreply.github.com>
2022-12-01 10:58:45 +0100

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <diV className="note">
+        <h1>This is the title</h1>
+        <p>This is the content</p>
+    </diV>
+}
+
+export default Note;
